e713b3756c2121df9ac47d3ea1ddf07dfe10be4e
kozake <sh.kozake@gmail.com>
2017-02-02 21:16:59 +0900

    Add business logic

--- src/main/java/com/github/kozake/sh/jhipster4/awesome/demo/repository/EntryRepository.java
+++ src/main/java/com/github/kozake/sh/jhipster4/awesome/demo/repository/EntryRepository.java
@@ -1,8 +1,10 @@
 package com.github.kozake.sh.jhipster4.awesome.demo.repository;
 
 import com.github.kozake.sh.jhipster4.awesome.demo.domain.Entry;
-
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -18,5 +20,7 @@ public interface EntryRepository extends JpaRepository<Entry,Long> {
 
     @Query("select entry from Entry entry left join fetch entry.tags where entry.id =:id")
     Entry findOneWithEagerRelationships(@Param("id") Long id);
+
+    Page<Entry> findByBlogUserLoginOrderByDateDesc(String currentUserLogin, Pageable pageable);
 
 }
